e998e2199608a0cbbeefaa23c5b81cf3c223ed08
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-28 15:15:41 +0530

    using "for" construct

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -21,4 +21,29 @@ func main() {
 		fmt.Println(no, "is Odd")
 	}
 	//fmt.Println("No is ", no) // will not work as the scope of the "no" variable is limited to "if else" block
+
+	/* For construct */
+	//version 1.0
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("sum = ", sum)
+
+	/* version 2.0 (while implementation) */
+	numSum := 1
+	for numSum < 100 {
+		numSum += numSum
+	}
+	fmt.Println("numSum =", numSum)
+
+	/* version 3.0 (infinite loop) */
+	x := 1
+	for {
+		x += x
+		if x > 100 {
+			break
+		}
+	}
+	fmt.Println("x =", x)
 }
