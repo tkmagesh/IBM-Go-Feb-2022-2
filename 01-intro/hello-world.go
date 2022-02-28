dc0ef3e2770d056cd008abbded2cabdb857fb7b0
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-28 13:58:27 +0530

    Data types in golang

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,17 @@
+/* package declaration (the package to which this go file belongs to) */
+/* package has to be "main" if this is the starting point of the application */
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package level declaration */
+
+/* package init function */
+
+/* main function (if this file is the starting point of the application) */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
