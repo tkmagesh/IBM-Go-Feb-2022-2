70d3dbf0bd0160bbb75ca508c5ab739615e528ee
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-28 17:19:52 +0530

    assingment:02 alternate solution

---
+++ 02-programming-constructs/assignment-02-b.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var userChoice int
+	var n1, n2, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice:")
+		fmt.Scanln(&userChoice)
+
+		switch userChoice {
+		case 1:
+			fmt.Println("Enter the operands (seperated by space):")
+			fmt.Scanf("%d %d\n", &n1, &n2)
+			result = n1 + n2
+			fmt.Printf("Result = %d\n", result)
+		case 2:
+			fmt.Println("Enter the operands (seperated by space):")
+			fmt.Scanf("%d %d\n", &n1, &n2)
+			result = n1 - n2
+			fmt.Printf("Result = %d\n", result)
+		case 3:
+			fmt.Println("Enter the operands (seperated by space):")
+			fmt.Scanf("%d %d\n", &n1, &n2)
+			result = n1 * n2
+			fmt.Printf("Result = %d\n", result)
+		case 4:
+			fmt.Println("Enter the operands (seperated by space):")
+			fmt.Scanf("%d %d\n", &n1, &n2)
+			result = n1 / n2
+			fmt.Printf("Result = %d\n", result)
+		case 5:
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid choice")
+		}
+
+	}
+}
