6bd835bef9c09a1b2de449512b41d902ca5cd2c3
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-02 15:53:10 +0530

    Slice - Part - 1

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -22,4 +22,36 @@ func main() {
 		fmt.Printf("Value @ [%d] = %d\n", idx, val)
 	}
 
+	fmt.Printf("\nSlice\n")
+	//var products []string
+	//var products []string = []string{"Pen", "Pencil", "Marker"}
+	//var products = []string{"Pen", "Pencil", "Marker"}
+	//products := []string{"Pen", "Pencil", "Marker"}
+
+	var products []string
+	//products[0] = "Pen"
+	//products = append(products, "Scribble-Pad")
+
+	fmt.Printf("\nMemory Allocation\n")
+
+	/* preallocation of memory */
+	products = make([]string, 0, 20)
+	fmt.Println(products)
+	/*
+		products = append(products, "Pen")
+		fmt.Printf("%v, len=%d, capacity=%d\n", products, len(products), cap(products))
+		products = append(products, "Pencil")
+		fmt.Printf("%v, len=%d, capacity=%d\n", products, len(products), cap(products))
+		products = append(products, "Marker")
+		fmt.Printf("%v, len=%d, capacity=%d\n", products, len(products), cap(products))
+		products = append(products, "Scribble-Pad")
+		fmt.Printf("%v, len=%d, capacity=%d\n", products, len(products), cap(products))
+		products = append(products, "Mouse")
+		fmt.Printf("%v, len=%d, capacity=%d\n", products, len(products), cap(products))
+		fmt.Println(products)
+	*/
+
+	data := make([]int, 5, 20)
+	fmt.Println(data, len(data), cap(data))
+
 }
