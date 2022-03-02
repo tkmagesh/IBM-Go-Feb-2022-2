7898157a145b69ffd4ca417fefbbf195a5d2b97f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-02 12:21:15 +0530

    assignment:05 solution

--- 03-functions/assignment-05.go
+++ 03-functions/assignment-05.go
@@ -9,12 +9,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(countPrimes(3, 4, 5, 6, 7, 8, 9, 12, 11, 17, 21))
+	fmt.Println("Prime Count = ", count(isPrime, 3, 4, 5, 6, 7, 8, 9, 12, 11, 17, 21))
+	fmt.Println("Even Count =", count(func(x int) bool {
+		return x%2 == 0
+	}, 3, 4, 5, 6, 7, 8, 9, 12, 11, 17, 21))
+	fmt.Println("Odd Count =", count(isOdd, 3, 4, 5, 6, 7, 8, 9, 12, 11, 17, 21))
 }
 
-func countPrimes(nos ...int) (count int) {
+func count(predicate func(int) bool, nos ...int) (count int) {
 	for i := 0; i < len(nos); i++ {
-		if isPrime(nos[i]) {
+		if predicate(nos[i]) {
 			count++
 		}
 	}
@@ -28,4 +32,8 @@ func isPrime(no int) bool {
 		}
 	}
 	return true
+}
+
+func isOdd(x int) bool {
+	return x%2 != 0
 }
