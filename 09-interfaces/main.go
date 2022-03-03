c96911c1ad6864f4643f1ea799807158d758b856
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-03 17:05:35 +0530

    interface composition

--- 09-interfaces/main.go
+++ 09-interfaces/main.go
@@ -13,6 +13,10 @@ func (c Circle) Area() float32 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float32 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Rectangle struct {
 	Height float32
 	Width  float32
@@ -20,6 +24,10 @@ type Rectangle struct {
 
 func (r Rectangle) Area() float32 {
 	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float32 {
+	return 2 * (r.Height * r.Width)
 }
 
 //utilities
@@ -38,12 +46,39 @@ func PrintArea(sa ShapeWithArea) {
 	Perimeter of rectangle = 2 * (height + width)
 */
 
+type ShapeWithPerimeter interface {
+	Perimeter() float32
+}
+
+func PrintPerimeter(sp ShapeWithPerimeter) {
+	fmt.Println("Perimeter = ", sp.Perimeter())
+}
+
+//interface composition
+type Shape interface {
+	ShapeWithArea
+	ShapeWithPerimeter
+}
+
+func PrintShape(s Shape) {
+	PrintArea(s)
+	PrintPerimeter(s)
+}
+
 func main() {
 	c := Circle{Radius: 10}
 	//fmt.Println("Area =", c.Area())
-	PrintArea(c)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintShape(c)
 
 	r := Rectangle{Height: 10, Width: 12}
 	//fmt.Println("Area =", r.Area())
-	PrintArea(r)
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintShape(r)
 }
