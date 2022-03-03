21080abfd6d97f25e9d7e3d3f448bdc4c3240d79
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-03 11:41:22 +0530

    Update main.go

--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	/* "modularity-demo/calculator" */
 
-	"modularity-demo/calculator"
+	calc "modularity-demo/calculator"
 	"modularity-demo/calculator/utils"
 )
 
@@ -14,9 +14,9 @@ func main() {
 		fmt.Println(calculator.Subtract(100, 200))
 		fmt.Println("OpCount = ", calculator.OpCount())
 	*/
-	fmt.Println(calculator.Add(100, 200))
-	fmt.Println(calculator.Subtract(100, 200))
-	fmt.Println("OpCount = ", calculator.OpCount())
+	fmt.Println(calc.Add(100, 200))
+	fmt.Println(calc.Subtract(100, 200))
+	fmt.Println("OpCount = ", calc.OpCount())
 	fmt.Println("Is 201 even ? ", utils.IsEven(201))
 	fmt.Println("Is 201 odd ? ", utils.IsOdd(201))
 }
