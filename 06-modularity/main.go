f9188a50fe35630f4c1b066a29f1156e7d324dd9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-03 12:09:07 +0530

    updates

--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -6,6 +6,8 @@ import (
 
 	calc "modularity-demo/calculator"
 	"modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -16,7 +18,8 @@ func main() {
 	*/
 	fmt.Println(calc.Add(100, 200))
 	fmt.Println(calc.Subtract(100, 200))
-	fmt.Println("OpCount = ", calc.OpCount())
+	color.Green(fmt.Sprintf("OpCount = %d\n", calc.OpCount()))
 	fmt.Println("Is 201 even ? ", utils.IsEven(201))
 	fmt.Println("Is 201 odd ? ", utils.IsOdd(201))
+	color.Red("This line will be printed in red color")
 }
