fbd7f31bb116a441ce9956a30d20e6ff38894448
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-04 09:49:20 +0530

    concurrency intro

---
+++ 10-concurrency/demo-01.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main started")
+	go f1()
+	f2()
+	time.Sleep(1 * time.Millisecond)
+	fmt.Println("main completed")
+}
+
+func f1() {
+	fmt.Println("f1 invoked")
+}
+
+func f2() {
+	fmt.Println("f2 invoked")
+}
